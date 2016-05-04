1a8c045d62ac97a4813deb689c93efe6e8a4300b
Avi <avi.samloff@gmail.com>
2016-05-04 12:57:59 -0700

    changes routing for source files

--- server/server.js
+++ server/server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 const clientDir = path.join(__dirname, '..', 'client');
-const angularDir = path.join(__dirname, '..', 'node_modules', 'angular');
+const nodemodules = path.join(__dirname, '..', 'node_modules');
 
 const apiRouter = require('./router.js');
 
 app.use(morgan('dev'));
 app.use('/', express.static(clientDir));
 
-app.use('/src/angular', express.static(angularDir));
+app.use('/src', express.static(nodemodules));
 
 app.use('/api', apiRouter);
 
