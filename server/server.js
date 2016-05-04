4afdc40b740e15b01e2ca7ccdf39cc2df649251b
Avi <avi.samloff@gmail.com>
2016-05-04 09:13:15 -0700

    creates api router

--- server/server.js
+++ server/server.js
@@ -7,9 +7,13 @@ const port = process.env.PORT || 8080;
 const clientDir = path.join(__dirname, '..', 'client');
 const angularDir = path.join(__dirname, '..', 'node_modules', 'angular');
 
+const apiRouter = require('./router.js');
+
 app.use(morgan('dev'));
 app.use('/', express.static(clientDir));
 
 app.use('/src/angular', express.static(angularDir));
+
+app.use('/api', apiRouter);
 
 app.listen(port, () => { console.log('listening on', port) });
