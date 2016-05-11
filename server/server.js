dda9c057bebadbc3cda0c9f70ca14e53500aec2b
Avi <avi.samloff@gmail.com>
2016-05-11 00:32:01 -0700

    allows Riot verification

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 8080;
 const clientDir = path.join(__dirname, '..', 'client');
 const nodemodules = path.join(__dirname, '..', 'node_modules');
 
+const verificationKey = process.env.verificationKey;
 const apiRouter = require('./router.js');
 
 app.use('/', express.static(clientDir));
+app.use('/', express.Router().get('/riot.txt', (req, res) => { res.send(verificationKey) }))
 
 app.use('/src', express.static(nodemodules));
 
